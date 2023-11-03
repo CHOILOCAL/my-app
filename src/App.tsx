b8baef7029235806bd3647c626d9a14ebe46f949
CHOILOCAL <hyunjchoi@hist.co.kr>
2023-11-03 17:25:09 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,13 @@ function App() {
     <BrowserRouter>
       <Header />
       <h1>test</h1>
-      <Routes>
+
+      {/* <Routes>
 					<Route path="/" element={<Main />}></Route>
 					<Route path="/product/*" element={<Product />}></Route>
-					{/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
 					<Route path="*" element={<NotFound />}></Route>
 				</Routes>
+         */}
       <Footer />
       </BrowserRouter>
     </>
