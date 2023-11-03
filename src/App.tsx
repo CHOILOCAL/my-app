e6e1528c4cf8b71e56d91b69e5ced4eb43841dc1
CHOILOCAL <hyunjchoi@hist.co.kr>
2023-11-03 15:55:48 +0900

    router 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import Footer from 'pages/Footer';
 import Header from 'pages/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Header />
       <h1>test</h1>
+      <Routes>
+					<Route path="/" element={<Main />}></Route>
+					<Route path="/product/*" element={<Product />}></Route>
+					{/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
+					<Route path="*" element={<NotFound />}></Route>
+				</Routes>
       <Footer />
+      </BrowserRouter>
     </>
   );
 }
